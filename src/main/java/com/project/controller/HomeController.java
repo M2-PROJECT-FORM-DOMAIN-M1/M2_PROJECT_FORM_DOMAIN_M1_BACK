3d09616b0f23a51876163777c07eaef8669bfe05
Florian Leclercq <florian.leclercq@isen.yncrea.fr>
2021-10-12 17:11:45 +0200

    [FLQ] add forget files from previous commit

--- src/main/java/com/project/controller/HomeController.java
+++ src/main/java/com/project/controller/HomeController.java
@@ -4,12 +4,13 @@ package com.project.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController extends AbstractController{
 
 
-    @GetMapping("/")
+    @PostMapping("/getCode")
     public ResponseEntity<?> test(){
         return  ResponseEntity.ok("toto");
     }
