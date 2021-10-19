2e53e996f108918e9f788dc7207d6ca4b6130438
Flavien <flavien.desse@student.junia.com>
2021-10-19 11:43:25 +0200

    [FDE] End init db test

--- src/main/java/com/project/configuration/CoreConfig.java
+++ src/main/java/com/project/configuration/CoreConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!Test")
 @Configuration
 @ImportResource({"classpath:/config/datasource.xml"})
 public class CoreConfig {
