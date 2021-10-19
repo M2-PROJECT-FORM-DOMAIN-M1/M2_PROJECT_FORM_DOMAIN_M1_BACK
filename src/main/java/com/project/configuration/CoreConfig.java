3a7c275c2b78ac1e00fe95c395c446d3013d2ec7
Flavien <flavien.desse@student.junia.com>
2021-10-19 09:36:29 +0200

    [FDE] Config test db

--- src/main/java/com/project/configuration/CoreConfig.java
+++ src/main/java/com/project/configuration/CoreConfig.java
@@ -2,7 +2,9 @@ package com.project.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!Test")
 @Configuration
 @ImportResource({"classpath:/config/datasource.xml"})
 public class CoreConfig {
